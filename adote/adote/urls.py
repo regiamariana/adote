797b46cda9681852d2b5a49c47408e8f99ae7dfb
mariana regia <guirado.mariana03@gmail.com>
2023-04-21 22:04:31 -0300

    up

--- adote/adote/urls.py
+++ adote/adote/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    # path('/', include('paginainicial.url')),
     path('admin/', admin.site.urls),
     path('auth/', include('usuarios.urls')),
     # include serve para apontar para determinado app
